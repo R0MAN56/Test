947f2b562adcefa7b7ed7187a0b300c9fa2a22e0
Roman <roman1610@mail.ru>
2023-12-31 14:05:07 +0500

    Задача 2

--- Desktop/GeekBrains/1_Задания/GIT/01_CSharp/Lesson2/Task6/Program.cs
+++ Desktop/GeekBrains/1_Задания/GIT/01_CSharp/Lesson2/Task6/Program.cs
@@ -1,11 +1,33 @@
 ﻿// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
 
-int num = 322;
-if (num % 7 == 0 && num % 23 == 0)
+// int num = 322;
+// if (num % 7 == 0 && num % 23 == 0)
+// {
+//     Console.WriteLine($"a={num}\t=> да");
+// }
+// else
+// {
+//     Console.WriteLine($"a={num}\t=> нет");
+// }
+
+
+// Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+int x = 4;
+int y = -2;
+if (x > 0 && y > 0)
 {
-    Console.WriteLine($"a={num}\t=> да");
+    Console.WriteLine($"{x},\t{y}\t=>\t1");
 }
-else
+else if (x < 0 && y > 0)
 {
-    Console.WriteLine($"a={num}\t=> нет");
+    Console.WriteLine($"{x},\t{y}\t=>\t2");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine($"{x},\t{y}\t=>\t3");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine($"{x},\t{y}\t=>\t4");
 }
