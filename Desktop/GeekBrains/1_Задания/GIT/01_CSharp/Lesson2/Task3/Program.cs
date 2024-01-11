fb29a617d9988f6bee8d3030a6703e3693addc85
Roman <roman1610@mail.ru>
2024-01-11 17:20:41 +0500

    add Task2,Task3,Task4

---
+++ Desktop/GeekBrains/1_Задания/GIT/01_CSharp/Lesson2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//int n = 10;
+int[] array = { 1, 2, 3, 5, 6, 7, 8, 9, 6 };
+int i = 0;
+
+while (i < array.length)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write(array[i]);
+    }
+    i = i + 1;
+}
